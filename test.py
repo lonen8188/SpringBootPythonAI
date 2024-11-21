1b9d6bc02cbbe0edf1b01d3db00df4daffd51dda
lonen8188 <112685332+lonen8188@users.noreply.github.com>
2024-11-21 09:35:50 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import requests
 
-url = "http://127.0.0.1:8000/detect"
-message = "Test message"
-file_path = "sample.jpg"
+url = "http://127.0.0.1:8000/detect" # 요청보낼 url
+message = "Test message"             # 서버로 전송할 메시지(폼 데이터로 전송)
+file_path = "sample.jpg"             # 전송할 이미지 파일 경로(py와 같은 경로에 존재)
 
 with open(file_path, "rb") as file: # https://velog.io/@37writes/with-openfilepath-rb-as-f-%EC%9D%98-%EC%9D%98%EB%AF%B8
     response = requests.post(url, data={"message": message},
